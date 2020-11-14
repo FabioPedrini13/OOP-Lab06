18fe30d82b4d793f0f4743e7a7dcd769b534c72a
Fabio Pedrini <fabio.pedrini3@studio.unibo.it>
2020-11-14 12:05:50 +0100

    Added Tests

--- src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
+++ src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
@@ -23,5 +23,17 @@ public class TestStrictBankAccount {
          * presenza di un id utente errato, oppure al superamento del numero di
          * operazioni ATM gratuite.
          */
+    	AccountHolder holder1 = new AccountHolder("Mario", "Rossi", 1);
+    	AccountHolder holder2 = new AccountHolder("Luigi", "Verdi", 2);
+    	StrictBankAccount account1 = new StrictBankAccount(1, 10000, 10);
+    	StrictBankAccount account2 = new StrictBankAccount(2, 10000, 10);
+    	
+    	account1.deposit(holder1.getUserID(), 2000);
+    	for (int i=0; i<9; i++) {
+    		account1.withdrawFromATM(holder1.getUserID(), 800);
+    		System.out.println("Balance= " + account1.getBalance() 
+    			+ "  nTransaction= " + account1.getNTransactions());
+    	}
+    	account2.withdraw(holder2.getUserID(), 2000);
     }
 }
